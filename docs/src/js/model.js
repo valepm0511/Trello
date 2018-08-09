c704f3f9339837abb54a9fd02267dfae7cbe8bd2
valeria <valeria.penaloza5@gmail.com>
2018-08-09 12:55:52 -0400

    version 0.0.6 a medias

--- docs/src/js/model.js
+++ docs/src/js/model.js
@@ -1,22 +1,21 @@
 window.model = {};
 // funcion para validar que a la lista se le ponga un nombre
 window.model.validateLengthNewList = (valueListNew) =>{
-    if (valueListNew.length > 0){
-      return true;
+	if (valueListNew.length > 0){
+		return true;
 	}else{
 		// alert("debe tener contenido");
 		return false;
 	}
-
 }
 
 // funcion para validar que textarea no qyede vacio
 window.model.validateLengthTextArea = (valueTextArea) =>{
 	if(valueTextArea.length > 0){
 		return true;
-  }
-  else
-  {
+	}
+	else
+	{
 		// alert("debe tener contenido");
 		return false;
 	}
--- src/js/view.js
+++ src/js/view.js
@@ -3,14 +3,13 @@ window.view = {};
 window.view.addList = () => {
   let divAddList = document.getElementById('addList');
   divAddList.innerHTML =
-  `<div class="col-12 bg-white p-3 contAddList">
+  `<div class="col-12 bg-white p-3  mb-3 contAddList">
   <form class="form-inline">
   <input type="text" class="form-control mb-2 col-12" id="nameList" placeholder="Ingrese nombre de la Lista" required>
   </form>
   <button class="btn btn-info" onclick="window.view.addNewList()">Añadir</button>
   <button class="btn btnCloseList" onclick="window.view.closeList()"><i class="fas fa-times"></i></button>
-  </div>
-  `;
+  </div>`;
   window.model.focusInput(document.getElementById('nameList'));
 };
 
@@ -19,7 +18,7 @@ window.view.closeList = () => {
   let divAddList = document.getElementById('addList');
   divAddList.innerHTML =
   `<button type="button" class="btn col-12 btnAddList" onclick="window.view.addList()"><span class="float-left">
-  <i class="fas fa-plus"></i> Añadir una lista</span></button>`;
+  <i class="fas fa-plus"></i>Añadir una lista</span></button>`;
 };
 
 // funcion para crear las listas 
@@ -28,22 +27,19 @@ window.view.addNewList = () => {
   // funcion para validar que a la lista se le ponga un nombre
   if (window.model.validateLengthNewList(nameList)){
     document.getElementById('nameList').value = '';
-    document.getElementById('newList').classList.remove('displayNone');
-    document.getElementById('newList').classList.add('displayBlock');
     let divNewList = document.getElementById('newList');
     let cantDiv = document.getElementsByClassName("contNewList").length;
-    console.log(cantDiv);
+    console.log("cantidad de div",cantDiv);
     divNewList.innerHTML += 
-    `<div class="col-12 pt-3 pb-3 contNewList">
+    `<div class="col-3 pt-3 pb-3 mb-3 contNewList order-1 ml-3">
     <p class="d-inline text-white">${nameList}</p>
-    <button class="btn float-right btnNewListIcon" onclick="window.view.deleteList()"><i class="fas fa-ellipsis-h text-white"></i></button>
+    <button class="btn float-right btnNewListIcon"><i class="fas fa-ellipsis-h text-white"></i></button>
     <div id="newListTextArea${cantDiv}">
     <button class="btn mt-3 col-12 btnNewList px-0 displayBlock" onclick="window.view.newList(${cantDiv})" id="btnaddTextArea"><i class="fas fa-plus"></i> Añadir nueva Tarea</button>
     </div>
     <div id="buttonTextArea${cantDiv}">
     </div>
-    </div>
-    `;
+    </div>`;
   }
 };
 
@@ -79,12 +75,4 @@ window.view.viewTextArea = (codID) => {
     </div>
     `;*/
   }
-};
-
-// funcion para eliminar lista
-window.view.deleteList = () => {
-  let deleteList = document.getElementById('newList');
-  document.getElementById('newList').classList.remove('displayBlock');
-  document.getElementById('newList').classList.add('displayNone');
-  deleteList.innerHTML = '';
 };
